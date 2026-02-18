2d8e227a41f9190c38ce80108326904db486d958
Bharat Bhusan Mohanty <bhusanb268@gmail.com>
2026-02-18 14:04:28 +0530

    median number finder

--- median_of_number_in_list.py
+++ median_of_number_in_list.py
@@ -1,9 +1,15 @@
-def kthSmallestNumber(number):
-    print(number[Kth-1])
-N=input().split(",")
-Kth=int(input())
-number=[]
-for item in N:
-    number+=[int(item)]
-number=sorted(number)
-kthSmallestNumber(number)
+def medianOfNumber(string_to_number,lenth_of_list):
+    if(lenth_of_list%2==0):
+        midel_index=lenth_of_list//2 
+        result=sum(string_to_number[midel_index - 1 :midel_index + 1])/2
+    else:
+        midel_index=lenth_of_list//2
+        result=string_to_number[midel_index]
+    print(result)
+the_input=input().split(",")
+string_to_number=[]
+for item in the_input:
+    string_to_number+=[int(item)]
+string_to_number=sorted(string_to_number,reverse=False)
+lenth_of_list=len(string_to_number)
+medianOfNumber(string_to_number,lenth_of_list)
